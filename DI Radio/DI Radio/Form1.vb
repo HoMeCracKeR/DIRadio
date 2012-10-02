73d9c45b9e04e5cc281b4098c53c7703419a1f64
Rodrigo Bernardi <newvirus@live.com.ar>
2012-10-02 15:15:43 -0300

    I forgot to update the version numbering.

--- DI Radio/DI Radio/Form1.vb
+++ DI Radio/DI Radio/Form1.vb
@@ -114,7 +114,7 @@ Public Class Form1
     Public AtStartup As String = False          ' -> Used to tell the GetUpdates background worker that it's looking for updates at startup. Only becomes True if UpdatesAtStart is true
     Public TotalVersionString As String         ' -> Used to store the TotalVersion returned by the server
     Public LatestVersionString As String        ' -> Used to store the actual version number returned by the server
-    Public TotalVersionFixed As Integer = 17    ' -> For commodity, I don't use the actual version number of the application to know when there's an update. Instead I check if this number is higher.
+    Public TotalVersionFixed As Integer = 18    ' -> For commodity, I don't use the actual version number of the application to know when there's an update. Instead I check if this number is higher.
 
 #End Region
 
